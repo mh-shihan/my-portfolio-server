3057f6851738fe04e26803a2a153d5c7bf47aee9
mh-shihan <mahmodul.no.1@gmail.com>
2025-04-05 15:00:54 +0600

    Created api for deleteing messages

--- index.js
+++ index.js
@@ -122,6 +122,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELETE API
+    app.delete("/messages/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await messageCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
